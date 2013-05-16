ecafed4c162d45c99e033eb2951d9e6825573bde
Tom Allen <tom.allen@bigcommerce.com>
2013-05-16 14:31:21 +1000

    Better formatting.

--- xisthenewy.rb
+++ xisthenewy.rb
@@ -3,17 +3,21 @@ require 'open-uri'
 
 root_uri = 'https://www.google.com/search?q='
 middle_query = '+is+the+new+'
-words = ['cheese','drug','sausage']
+words = ['white','black','drug','ruby','java']
 
-print "\t#{words.join("\t")}\n"
+print "X \\ Y\t#{words.join("\t")}\n"
 words.each do |x|
   print "#{x}\t"
   words.each do |y|
-    query_string = "#{root_uri}#{x}#{middle_query}#{y}"
-    doc = Nokogiri::HTML(open(query_string))
-    results_string = doc.at_css('#resultStats').children.first.text
-    results_count = /^About (.*) results$/.match(results_string)[1].delete(',').to_f
-    printf "%.1E\t", results_count
+    if x != y
+      query_string = "#{root_uri}#{x}#{middle_query}#{y}"
+      doc = Nokogiri::HTML(open(query_string))
+      results_string = doc.at_css('#resultStats').children.first.text
+      results_count = /^About (.*) results$/.match(results_string)[1].delete(',').to_f
+      printf "%.1E\t", results_count
+    else
+      print "\t"
+    end
   end
   print "\n"
 end
